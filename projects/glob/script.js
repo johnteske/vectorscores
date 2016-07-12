0e7cbcfa32d06e44979ebd9967e2c21dff5f50cc
johnteske <john.teske@gmail.com>
2016-07-11 20:52:20 -0700

    Prepare glob for responsive sizing

--- projects/glob/script.js
+++ projects/glob/script.js
@@ -9,7 +9,7 @@ var globWidth = 240,
 var noteheads = [
     // "M397 -1597q0 12 6 30q49 131 49 305t-56 302t-179 285t-217 173v490q0 31 20 30q53 0 62 -45q43 -254 223 -534q236 -375 236 -703q0 -143 -41 -301l-13 -53q-6 -25 -26 -35t-31 -4q-33 23 -33 60z", // eighth flag
     "M223 -289q-96 0 -160 52q-63 52 -63 141q0 152 134 268t321 117q100 0 162 -53t63 -140q0 -143 -150 -264t-307 -121z", // quarter
-    "M625 96q0 28 -15 54q-30 49 -98 49t-227 -93q-215 -123 -215 -210q0 -23 14 -48q31 -53 107 -53t219 96q215 141 215 205zM234 -297q-106 0 -170 52t-64 153t98 235q50 66 146 110t210 44t180 -54q64 -54 64 -139t-43 -161t-91 -124t-136 -82t-194 -34z" // half
+    // "M625 96q0 28 -15 54q-30 49 -98 49t-227 -93q-215 -123 -215 -210q0 -23 14 -48q31 -53 107 -53t219 96q215 141 215 205zM234 -297q-106 0 -170 52t-64 153t98 235q50 66 146 110t210 44t180 -54q64 -54 64 -139t-43 -161t-91 -124t-136 -82t-194 -34z" // half
 ];
 
 function chooseNotehead() {
@@ -17,10 +17,8 @@ function chooseNotehead() {
 }
 
 var main = d3.select(".main")
-    .attr("width", width)
-    .attr("height", height);
-
-// main.classed("debug", true);
+    .style('width', width + 'px')
+    .style('height', width + 'px');
 
 function newPoint() {
     var angle = Math.random()*Math.PI*2;
@@ -49,16 +47,6 @@ for (var i = 0; i < cloudSize; i++) {
         });
     }
 
-// main.append("circle")
-//     .attr("stroke", "red")
-//     .attr("fill", "white")
-//     .attr("r", 5)
-//     .attr("transform", function(d, i) {
-//         var x = center
-//             y = center
-//         return "translate(" + x + ", " + y + ")";
-//     });
-
 main.append("text")
     .attr("fill", "#111")
     .attr("text-anchor", "middle")
@@ -83,3 +71,16 @@ function moveIt(){
 moveIt();
 
 setInterval(function(){moveIt()}, dur);
+
+// DEBUG
+
+main.classed("debug", true);
+main.append("circle")
+    .attr("stroke", "red")
+    .attr("fill", "white")
+    .attr("r", 5)
+    .attr("transform", function(d, i) {
+        var x = center
+            y = center
+        return "translate(" + x + ", " + y + ")";
+    });
