b5fd4426e96489df08d5bd1efaff5fe8a832c459
johnteske <john.teske@gmail.com>
2016-11-13 17:41:08 -0800

    Use schedule() to delay clearing of spans on stop()

--- projects/time-control/script.js
+++ projects/time-control/script.js
@@ -11,7 +11,7 @@ schedule(1000, play, "no params");
 
 function userEvent(ndex) {
 	var id = scoreEvents.events[ndex];
-	setSpanActive(id, ndex);
+	setSpanActive(id);
 }
 
 // create events
@@ -24,29 +24,30 @@ function createSpan(eventTime){
 	document.getElementById("events").appendChild(spanel);
 }
 
-// create first event
-scoreEvents.add(0);
-createSpan(0);
-
-// create remaining events
-for (var i = 0; i < numvents; i++) {
-	var eventTime = Math.floor(Math.random()*500)+(i*1500)+1000;
-	scoreEvents.add(eventTime);
+function createEvent(eventTime, eventFunc, eventParams) {
+	scoreEvents.add(eventTime); // scoreEvent -- [time, function, params]
 	createSpan(eventTime);
+}
+createEvent(0); // create first event
+for (var i = 0; i < numvents; i++) { // create remaining events
+	var eventTime = Math.floor(Math.random()*500)+(i*1500)+1000;
+	createEvent(eventTime);
 }
 
 // activate span
-function setSpanActive(id, ndex){
+function setSpanActive(id){
 	var thing = document.getElementById(id);
-	console.log('event ', ndex, ': ', id);
 	thing.setAttribute("class", "active");
 }
 
 function userStop() {
-	// on stop, clear active classes from elements
-	var spanz = document.getElementsByTagName("span");
-	for (var i = 0; i < spanz.length; i++) {
-		var thisspan = spanz[i];
-		thisspan.className = '';
+	function clearSpans(){
+		// on stop, clear active classes from elements
+		var spanz = document.getElementsByTagName("span");
+		for (var i = 0; i < spanz.length; i++) {
+			var thisspan = spanz[i];
+			thisspan.className = '';
+		}
 	}
+	schedule(3000, clearSpans, "no params");
 }
