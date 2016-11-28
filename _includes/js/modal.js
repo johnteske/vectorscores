96d89403addfc649b031de2b009df63b8a7a5d1e
johnteske <john.teske@gmail.com>
2016-11-27 22:21:23 -0800

    Add VS.modal and functionality only if modal exists

--- _includes/js/modal.js
+++ _includes/js/modal.js
@@ -1,38 +1,41 @@
-VS.modal = (function () {
+if (document.getElementById("score-info-modal")) {
+    VS.modal = (function () {
 
-    var infoOpen = document.getElementById("score-info"),
-        infoClose = document.getElementById("score-info-modal-close");
+        var overlay = document.getElementById("score-info-modal"),
+            infoOpen = document.getElementById("score-info"),
+            infoClose = document.getElementById("score-info-modal-close");
 
-    infoOpen.onclick = openInfoModal;
-    infoClose.onclick = closeInfoModal;
+        infoOpen.onclick = openInfoModal;
+        infoClose.onclick = closeInfoModal;
 
-    function openInfoModal() {
-        VS.modal.overlay.style.display = "block";
-        VS.page.headerClassed("show");
-        VS.page.footerClassed("show");
-        if (VS.page.footer) {
-            VS.score.pause();
+        function openInfoModal() {
+            VS.modal.overlay.style.display = "block";
+            VS.page.headerClassed("show");
+            VS.page.footerClassed("show");
+            if (VS.page.footer) {
+                VS.score.pause();
+            }
         }
-    }
 
-    function closeInfoModal() {
-        VS.modal.overlay.style.display = "none";
-        VS.page.headerClassed("");
-        VS.page.footerClassed("");
-    }
-
-    window.onclick = function(event) { // addEventHandler?
-        if (event.target === VS.modal.overlay) { closeInfoModal(); }
-    };
-
-    return {
-        overlay: document.getElementById("score-info-modal"),
-        info: {
-            openElement: infoOpen,
-            closeElement: infoClose,
-            open: openInfoModal,
-            close: closeInfoModal
+        function closeInfoModal() {
+            VS.modal.overlay.style.display = "none";
+            VS.page.headerClassed("");
+            VS.page.footerClassed("");
         }
-    };
 
-})();
+        window.onclick = function(event) { // addEventHandler?
+            if (event.target === VS.modal.overlay) { closeInfoModal(); }
+        };
+
+        return {
+            overlay: overlay,
+            info: {
+                openElement: infoOpen,
+                closeElement: infoClose,
+                open: openInfoModal,
+                close: closeInfoModal
+            }
+        };
+
+    })();
+}
