e3092c2011093c1ead0f403eacf3c1679420a033
johnteske <john.teske@gmail.com>
2016-11-30 23:29:21 -0800

    Display topo test in isometric grid
    Look into using document.createDocumentFragment()
    to create new rows and columns for scrolling.

--- projects/topo/script.js
+++ projects/topo/script.js
@@ -4,10 +4,9 @@ var main = d3.select(".main"),
 var width = 480,
     // height = 480,
     rows = 8,
-    cols = 12,
-    cellSize = 25,
-    cellSpacing = cellSize,
-    rowOffset = cellSize * -0.5;
+    cols = 8,
+    tileWidthHalf = 16,
+    tileHeightHalf = 8;
 
 var dict = {
     0: ".",
@@ -18,52 +17,31 @@ var dict = {
 main.style("width", width + "px")
     .style("height", width + "px");
 
-// main.classed("debug", 1);
+// function createDataset(rows,cols){
+//     var _array = [];
+//     for(var i = 0; i < rows; i++){ // row
+//         _array[i] = [];
+//         for(var j = 0; j < cols; j++){ // col
+//             _array[i][j] = VS.getItem([0,1,2]);
+//         }
+//     }
+//     return _array;
+// }
+//
+// var dataset = createDataset(rows,cols);
 
-function createDataset(rows,cols){
-    var _array = [];
-    for(var i = 0; i < rows; i++){ // row
-        _array[i] = [];
-        for(var j = 0; j < cols; j++){ // col
-            _array[i][j] = VS.getItem([0,1,2]);
-        }
+for(var i = 0; i < rows; i++){ // row
+    for(var j = 0; j < cols; j++){ // col
+        topo.append("text")
+        .text(function() {
+            return dict[VS.getWeightedItem([0,1,2],[0.55,0.05,0.4])];
+        })
+        .attr("transform", function() {
+            return "translate(" +
+                ((j - i) * tileWidthHalf) + ", " +
+                ((j + i) * tileHeightHalf) + ")";
+        });
     }
-    return _array;
 }
 
-var dataset = createDataset(rows,cols);
-
-
-topo.selectAll("g")
-    .data(dataset)
-    // rows
-    .enter().append("g")
-    .attr("transform", function (d, i) {
-        return "translate(" + (i * rowOffset) + ", " + (i * cellSize) + ")";
-    })
-    // each column in row
-    .selectAll("text")
-    .data(function(d) {return d;})
-    .enter().append("text")
-    .text(function(d) {
-        return dict[d];
-    })
-    .attr("x", function(d, i) {
-        return i * cellSpacing;
-    });
-
-topo
-    .attr("transform",
-        "translate(120,120)"
-    );
-//     .transition()
-//     .delay(500)
-//     .duration(500)
-//     .attrTween("transform", tween);
-//     // .attr("transform",
-//         // "translate(120,120) rotate(-45)"
-//     // );
-//
-// function tween(d, i, a) {
-//   return d3.interpolateString("translate(120,120) rotate(0,60,60)", "translate(120,150) rotate(-45,60,60)");
-// }
+topo.attr("transform", "translate(240,120)");
