53100130d6f5ef6732e76caea352255ae3262741
John Teske <john.teske@gmail.com>
2017-01-19 20:28:45 -0800

    Add durations to phrases

--- projects/adsr/script.js
+++ projects/adsr/script.js
@@ -10,32 +10,37 @@ var width = 640,
 function lerp(v0, v1, t) {
     return (1 - t) * v0 + t * v1;
 }
-function getPrevNextIndicesAndT(array, val) {
+function getPrevNextIndices(array, val) {
     for (var i = 0; i < array.length; i++) {
         if (val >= array[i-1] && val <= array[i]) {
-            return [i-1, i, val - array[i-1]];
+            return [i-1, i];
         }
     }
+}
+function getPrevNextIndicesAndT(array, val) {
+    var indices = getPrevNextIndices(array, val);
+    return [indices[0], indices[1], val - array[indices[0]]];
 }
 function roundHalf(num) {
     return Math.round(num*2)/2;
 }
 
-// // ~durs = [0.2,0.25,0.5,0.75,1,1.5,2,3,4,6,8];
+var durations = [0.2,0.25,0.5,0.75,1,1.5,2,3,4,6,8];
 var timbres = ["bartok", "pizz.", "ghost", "rolling pizz.", "bow hair pull", "sul pont.", "flutter", "vib.", "ord.", "l.v."];
 
 var envelopes = {
-    phraseLength: [1,1,2,3,4,1,1],
-    timeDispersion: [0,0,1,1.5,2,2.5,1],
+    phraseLength: [1, 1, 2, 3, 4, 1, 1],
+    timeDispersion: [0, 0, 1, 1.5, 2, 2.5, 1],
     pitch: {
-        high: [0,0,0.5,1,1.5,2,2],
-        low: [0,-0.5,-1,-1.5,-2,-2,-2]
+        high: [0, 0, 0.5, 1, 1.5, 2, 2],
+        low: [0, -0.5, -1, -1.5, -2, -2, -2]
     },
-    timbre: [0,2,4,5,6,8,9]
+    duration: {
+        high: [0.2, 0.75, 1.5, 3, 6, 4, 4],
+        low: [0.2, 0.5, 0.5, 1.0, 2, 3, 3]
+    },
+    timbre: [0, 2, 4, 5, 6, 8, 9]
 };
-
-// ~durhi = InterplEnv([0.2, 0.75, 1.5, 3, 6, 4, 4], m, [\lin]);
-// ~durlo = InterplEnv([0.2, 0.5,  0.5, 1.0, 2, 3, 3], m, [\lin]);
 
 // // ~score = [];
 // // 4.do({|part|
@@ -81,10 +86,25 @@ for (var i = 0; i < timePoints.length; i++) {
         notes = [];
 
         for (var j = 0; j < phraseLength; j++) {
-		// 	// var thisdur = rrand(~durlo.at(now), ~durhi.at(now));
-		// 	// thisdur = thisdur.nearestInList(~durs);
-		// 	// thisstring = thisstring.add(thisdur);
-            notes.push(1);
+            var highDur =
+                lerp(
+                    envelopes.duration.high[iit[0]],
+                    envelopes.duration.high[iit[1]],
+                    iit[2]
+                );
+            var lowDur =
+                lerp(
+                    envelopes.duration.low[iit[0]],
+                    envelopes.duration.low[iit[1]],
+                    iit[2]
+                );
+            // find a (random) duration between these envelopes
+            var randDur = VS.getRandExcl(lowDur, highDur);
+            // match that to the closest durations
+			var closeDurIndices = getPrevNextIndices(durations, randDur);
+            // and pick one of the two
+            var thisDur = durations[VS.getItem(closeDurIndices)];
+            notes.push(thisDur);
 		};
 
         phrase = [timeDispersion, notes, timbre, pitch.high, pitch.low];
