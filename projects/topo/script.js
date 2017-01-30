be622e9d5b7b2ce276e86cc687ea236434d741ed
John Teske <john.teske@gmail.com>
2017-01-29 22:03:58 -0800

    Create topographic score fragment-generating function

--- projects/topo/script.js
+++ projects/topo/script.js
@@ -1,53 +1,48 @@
+---
+---
 var main = d3.select(".main"),
-    topo = main.append("g");
-
-var width = 480,
+    topo = main.append("g"),
+    width = 480,
     // height = 480,
-    rows = 8,
-    cols = 8,
+    // rows = 8,
+    // cols = 8,
     tileWidthHalf = 16,
-    tileHeightHalf = 8;
-
-var dict = {
-    0: ".",
-    1: ">",
-    2: "-"
-};
+    tileHeightHalf = 8,
+    topoScore,
+    symbols = {
+        0: ".",
+        1: ">",
+        2: "-"
+    };
 
 main.style("width", width + "px")
     .style("height", width + "px");
 
-function createDataset(rows,cols){
-    var _array = [];
+{% include_relative _score.js %}
+
+function drawScore(scoreFragment, x, y) {
+    var documentFragment = document.createDocumentFragment(),
+        rows = scoreFragment.length,
+        cols = scoreFragment[0].length;
+
     for(var i = 0; i < rows; i++){ // row
-        _array[i] = [];
         for(var j = 0; j < cols; j++){ // col
-            _array[i][j] = VS.getWeightedItem([0,1,2],[0.55,0.05,0.4]);
+            d3.select(documentFragment).append("svg:text")
+            .text(function() {
+                return symbols[scoreFragment[i][j]];
+            })
+            .attr("transform", function() {
+                return "translate(" +
+                    ((x + (j - i)) * tileWidthHalf) + ", " +
+                    ((y + (j + i)) * tileHeightHalf) + ")";
+            });
         }
     }
-    return _array;
+
+    topo.node().appendChild(documentFragment);
 }
-var dataset = createDataset(rows,cols);
-
-
-var documentFragment = document.createDocumentFragment(),
-    df = d3.select(documentFragment);
-
-for(var i = 0; i < rows; i++){ // row
-    for(var j = 0; j < cols; j++){ // col
-        df.append("svg:text")
-        .text(function() {
-            return dict[dataset[i][j]];
-        })
-        .attr("transform", function() {
-            return "translate(" +
-                ((j - i) * tileWidthHalf) + ", " +
-                ((j + i) * tileHeightHalf) + ")";
-        });
-    }
-}
-
-topo.node().appendChild(documentFragment);
-
+topoScore = createScoreFragment(8,7);
+drawScore(topoScore, 0, 0);
+drawScore(createScoreFragment(8,1), 7,7);
 
 topo.attr("transform", "translate(240,120)");
