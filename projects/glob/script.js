c1da9ed040f35cca9a62c7676d7fade498edce48
John Teske <john.teske@gmail.com>
2017-02-19 16:43:25 -0800

    Lint glob project

--- projects/glob/script.js
+++ projects/glob/script.js
@@ -10,17 +10,11 @@ var globWidth = 240,
     maxwidth = 400,
     width = innerwidth + (margin * 2),
     center = width * 0.5,
-    scale = (width / 9600),
     tLong = 3000,
-    tShort = 1500;
-var textoffset = 5;
-var noteheads = [
-    // "M397 -1597q0 12 6 30q49 131 49 305t-56 302t-179 285t-217 173v490q0 31 20 30q53 0 62 -45q43 -254 223 -534q236 -375 236 -703q0 -143 -41 -301l-13 -53q-6 -25 -26 -35t-31 -4q-33 23 -33 60z", // eighth flag
-    "M223 -289q-96 0 -160 52q-63 52 -63 141q0 152 134 268t321 117q100 0 162 -53t63 -140q0 -143 -150 -264t-307 -121z", // quarter
-    // "M625 96q0 28 -15 54q-30 49 -98 49t-227 -93q-215 -123 -215 -210q0 -23 14 -48q31 -53 107 -53t219 96q215 141 215 205zM234 -297q-106 0 -170 52t-64 153t98 235q50 66 146 110t210 44t180 -54q64 -54 64 -139t-43 -161t-91 -124t-136 -82t-194 -34z" // half
-];
-var halfNoteheadWidth = 340;
-var debug = VS.getQueryString("debug") == 1 || false;
+    tShort = 1500,
+    scoreLength = 10,
+    textoffset = 5,
+    debug = VS.getQueryString("debug") == 1 || false;
 
 var main = d3.select(".main")
     .style("width", width + "px")
@@ -80,11 +74,11 @@ function moveIt(){
         .attr("transform", transformNotehead);
 }
 
-for(i = 0; i < 10; i++) {
+for(var i = 0; i < scoreLength; i++) {
     VS.score.add([i * tLong, moveIt]);
 }
 // final event
-VS.score.add([i * tLong, function() {
+VS.score.add([scoreLength * tLong, function() {
     d3.selectAll("text")
         .transition()
         .duration(tShort)
