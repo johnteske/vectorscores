37aec5dc2557e0abc5781be27aac3f9c8e0482ea
John Teske <john.teske@gmail.com>
2017-03-05 23:04:01 -0800

    Move perfomer angle indicator script to partial

--- projects/3d-object/script.js
+++ projects/3d-object/script.js
@@ -8,19 +8,8 @@ var main = d3.select(".main")
     .style("width", "240px")
     .style("height", "240px");
 
-var indicator = {
-    container: d3.select(".indicator svg")
-        .style("width", "80px")
-        .style("height", "80px"),
-    circle: {
-        r: 32,
-        x: 40,
-        y: 40
-    }
-};
-
 /**
- *  setAngle accepts degrees, saves value as radians
+ * setAngle accepts degrees, saves value as radians
  */
 function Performer() {
     var _angle,
@@ -31,19 +20,9 @@ function Performer() {
             // substract 90 degress so origin is top,
             // then convert to radians
             _angle = (newAngle - 90) * (Math.PI / 180);
-            // TODO these points are pixels, not relevant to the score size
-            // I'll need both to display the indicator and the score
-            _point.x = indicator.circle.x + (indicator.circle.r * Math.cos(_angle));
-            _point.y = indicator.circle.y + (indicator.circle.r * Math.sin(_angle));
         },
         getAngle: function() {
             return _angle;
-        },
-        x: function() {
-            return _point.x;
-        },
-        y: function() {
-            return _point.y;
         }
     };
 }
@@ -52,36 +31,5 @@ var performer = new Performer;
 // TODO allow this to be set by query string on load,
 // also update query string so bookmarks/share/reloads retain current settings
 performer.setAngle(45);
-performer.positionIndicator =
-    indicator.container.append("circle")
-        .classed("performer", 1)
-        .attr("cx", performer.x())
-        .attr("cy", performer.y())
-        .attr("r", 4);
 
-// circle
-indicator.container.append("circle")
-    .attr("cx", indicator.circle.x)
-    .attr("cy", indicator.circle.y)
-    .attr("r", indicator.circle.r);
-
-// "front" indicator (stage or agreed upon origin)
-indicator.container.append("path")
-    .attr("d",
-        "M" + indicator.circle.x + "," + (indicator.circle.y - indicator.circle.r - 8) +
-        "L" + indicator.circle.x + "," + (indicator.circle.y - indicator.circle.r + 8)
-    );
-
-/**
- *  HTML input control
- *  TODO also allow performer to set angle by clicking/touching, dragging the indicator
- */
-
-var performerAngleInput = document.getElementById("performer-angle");
-
-performerAngleInput.addEventListener("change", function() {
-     performer.setAngle(performerAngleInput.value);
-     performer.positionIndicator
-         .attr("cx", performer.x())
-         .attr("cy", performer.y());
-});
+{% include_relative _indicator.js %}
