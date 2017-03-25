1c182fe2501095e67ebb85104cdd8f4688644704
John Teske <john.teske@gmail.com>
2017-03-25 12:10:52 -0700

    Add basic settings modal, wrapping both modals in same overlay

--- _includes/js/modals.js
+++ _includes/js/modals.js
@@ -1,10 +1,12 @@
-VS.enableModal = function(openId, closeId, overlayId) {
-    var openTrigger = document.getElementById(openId),
+VS.enableModal = function(modalId, openId, closeId) {
+    var modal = document.getElementById(modalId),
+        openTrigger = document.getElementById(openId),
         closeTrigger = document.getElementById(closeId),
-        overlay = document.getElementById(overlayId);
+        overlay = document.getElementById("score-modal-overlay"); // wraps all score modals
 
     function openModal() {
         overlay.style.display = "block";
+        modal.style.display = "block";
         VS.page.headerClassed("show");
         VS.page.footerClassed("show");
         if (VS.page.footer) { VS.score.pause(); }
@@ -17,6 +19,7 @@ VS.enableModal = function(openId, closeId, overlayId) {
 
     function closeModal() {
         overlay.style.display = "none";
+        modal.style.display = "none";
         VS.page.headerClassed("");
         VS.page.footerClassed("");
 
@@ -47,6 +50,9 @@ VS.enableModal = function(openId, closeId, overlayId) {
 };
 
 // TODO this check belongs in document.ready
-if (document.getElementById("score-info-modal")) {
-    VS.enableModal("score-info", "score-info-modal-close", "score-info-modal");
+if (document.getElementById("score-info-open")) {
+    VS.enableModal("score-info-modal", "score-info-open", "score-info-close");
+}
+if (document.getElementById("score-settings-open")) {
+    VS.enableModal("score-settings-modal", "score-settings-open", "score-settings-close");
 }
