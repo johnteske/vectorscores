15a5bc2cec80a1e3be466206fc336f4cea67f2e6
John Teske <john.teske@gmail.com>
2017-06-10 16:14:08 -0700

    Add brackets to trash

--- scores/trashfire/_trash.js
+++ scores/trashfire/_trash.js
@@ -1,23 +1,37 @@
-var trash = [3, 5, 7, 9].map(function(size) {
+/**
+ * Generate test trash
+ */
+var trash = [30, 45, 70, 85].map(function(size) {
     return {
         size: size,
-        active: false
+        active: true
     };
 });
 
 function makeCircle(selection) {
     selection.append("circle")
-        .attr("cx", TrashFire.trashOrigin.x)
-        .attr("cy", TrashFire.trashOrigin.y)
-        .attr("r", function(d) { return d.size; });
+        .attr("fill-opacity", "0.5")
+        .attr("cx", function(d) { return d.size * 0.5; }) // .attr("cx", TrashFire.trashOrigin.x)
+        .attr("cy", function(d) { return d.size * 0.5; }) // .attr("cy", TrashFire.trashOrigin.y)
+        .attr("r", function(d) { return d.size * 0.5 - 5; });
 }
 
-trashContainer.selectAll(".trash")
-    .data(trash)
-    .enter()
-    .append("g")
-    .attr("class", "trash")
-        .call(makeCircle);
+var trashes = trashContainer.selectAll(".trash")
+    .data(trash).enter()
+    .append("g").attr("class", "trash")
+        .attr("transform", function (d, i) {
+            return "translate(" + i * 90 + "," + (d.size * -0.5) + ")";
+        });
+trashes.append("path")
+    .style("opacity", function(d) { return d.active ? 1 : 0; })
+    .attr("stroke", "grey")
+    .attr("fill", "none")
+    .attr("d", function(d) {
+        var w = 5, h = d.size;
+        return "M" + w + ",0 L0,0 L0," + h + " L" + w + "," + h + " " +
+            "M" + (h - w) + "," + h + "L" + h + "," + h + " L" + h + "," + 0 + " L" + (h - w) + "," + 0;
+    });
+trashes.call(makeCircle);
 
 // TrashFire.Trash = function(width, height) {
 //     var trash = {},
