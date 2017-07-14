1cdc0ec40198108f901d64d52b2626c0d35b3a65
John Teske <john.teske@gmail.com>
2017-07-14 08:17:42 -0700

    Stash alternate symbol set (accidentals) for topographies

--- scores/topographies-ii/script.js
+++ scores/topographies-ii/script.js
@@ -9,16 +9,27 @@ var main = d3.select(".main"),
     tileHeightHalf = tileWidthHalf * 0.5,
     topoScore,
     symbolOffsets = {
-        0.25: { x: -0.025, y: -0.25 }, // TODO
-        0.5: { x: -0.025, y: -0.25 },
-        1: { x: -0.175, y: 0 },
-        2:  { x: -0.175, y: 0 },
+        // 0.25: { x: -0.025, y: -0.25 }, // TODO
+        // 0.5: { x: -0.025, y: -0.25 },
+        // 1: { x: -0.175, y: 0 },
+        // 2:  { x: -0.175, y: 0 },
         ".": { x: -0.0625, y: 0.0625 },
         ">": { x: -0.1625, y: 0.125 },
-        "-": { x: -0.1625, y: 0.03125 }
+        "-": { x: -0.1625, y: 0.03125 },
+        //
+        "-2": { x: -0.2, y: 0 }, // double flat
+        "-1.5": { x: -0.225, y: 0 }, // three-quarter flat (backwards, forwards)
+        "-1": { x: -0.1, y: 0 }, // flat
+        "-0.5": { x: -0.1325, y: 0 }, // quarter flat (backwards)
+        "0": { x: -0.0875, y: 0 }, // natural
+        // "0.5":  "\ue282", // quarter sharp (single vertical stroke)
+        // "1":    "\ue262", // sharp
+        // "1.5":  "\ue283", // three-quarter sharp (three vertical strokes)
+        // "2":    "\ue263"  // double sharp
     };
 
-var symbols = Object.assign(VS.dictionary.Bravura.durations.stemless, VS.dictionary.Bravura.articulations);
+// var symbols = Object.assign(VS.dictionary.Bravura.durations.stemless, VS.dictionary.Bravura.articulations);
+var symbols = Object.assign(VS.dictionary.Bravura.accidentals, VS.dictionary.Bravura.articulations);
 
 main.style("width", 640 + "px")
     .style("height", 640 + "px");
@@ -34,7 +45,8 @@ function drawScore(scoreFragment, x, y) {
     for (var row = 0; row < rows; row++) {
         for (var col = 0; col < cols; col++) {
             var symbolIndex = scoreFragment[row][col] + 4;
-            var symbolKey = ["-", "-", ">", ".", 2, 1, 0.5, 0.25][symbolIndex];
+            // var symbolKey = ["-", "-", ">", ".", 2, 1, 0.5, 0.25][symbolIndex];
+            var symbolKey = ["-2", "-1.5", "-1", "-0.5", "0", "-", ">", "."][symbolIndex];
 
             // to help center symbols with offsets
             // d3.select(documentFragment).append("svg:path")
@@ -50,7 +62,6 @@ function drawScore(scoreFragment, x, y) {
             //     });
 
             var offsets = symbolOffsets[symbolKey];
-            // var offsets = {x: 0, y: 0};
 
             d3.select(documentFragment).append("svg:text")
                 // .text(symbolKey)
