1ab01a26e5739d4fea72316f0ddf927f943a8f2b
John Teske <john.teske@gmail.com>
2017-08-09 13:34:53 -0700

    Separate data manipulation and conversion

--- scores/topographies-ii/_score.js
+++ scores/topographies-ii/_score.js
@@ -1,20 +1,31 @@
 /**
+ * Assign properties to row-major order data
+ */
+function createScoreFragment(data, width, nRows, nCols) {
+    for (var i = 0; i < data.length; i++) {
+        var h = data[i];
+
+        data[i] = {
+            height: h,
+            heightIndex: ~~h // h > 0 ? Math.floor(h) : Math.ceil(h)
+        }
+    }
+
+    return rowMajorOrderToGrid(data, width, nRows, nCols);
+}
+
+/**
  * Convert row-major order data in two-dimensional array
  * and assign heightIndex for symbol matching
  */
-function createScoreFragment(data, width, nRows, nCols) {
+function rowMajorOrderToGrid(data, width, nRows, nCols) {
     var grid = [];
 
     for (var row = 0; row < nRows; row++) {
         grid[row] = [];
 
         for (var col = 0; col < nCols; col++) {
-            var height = data[col + row * width];
-
-            grid[row][col] = {
-                height: height,
-                heightIndex: ~~height // height > 0 ? Math.floor(height) : Math.ceil(height)
-            };
+            grid[row][col] = data[col + row * width];
         }
     }
 
