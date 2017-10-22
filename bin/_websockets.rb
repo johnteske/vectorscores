c00e3d292247699655f6c2ba559edd5e8904a238
John Teske <john.teske@gmail.com>
2017-10-22 12:20:14 -0700

    Lint ruby files

--- bin/_websockets.rb
+++ bin/_websockets.rb
@@ -1,70 +1,70 @@
-# TODO use paths for each piece
+# TODO: use paths for each piece
 
 require 'em-websocket'
 require 'json'
 
-module MyKeyboardHandler
+# Handle user input while server is running
+module KeyboardHandler
   include EM::Protocols::LineText2
 
   def initialize(c)
     @clients = c
   end
 
-  def receive_line data
-    if data =~ /reload/i
-      msg = ["", "ws", "reload"].to_json
+  def receive_line(data)
+    return unless data =~ /reload/i
 
+    msg = ['', 'ws', 'reload'].to_json
+
+    @clients.each do |socket|
+      socket.send msg
+    end
+  end
+end
+
+EM.run do
+  @clients = []
+
+  # Update all clients with number of total connections
+  def send_n_connections(cid = nil)
+    puts "#{@clients.length} connections open"
+
+    msg = [cid, 'ws', 'n', @clients.length].to_json
+
+    @clients.each do |socket|
+      socket.send msg
+    end
+  end
+
+  EM::WebSocket.start(host: '0.0.0.0', port: '4001') do |ws|
+    ws.onopen do |handshake|
+      puts 'WebSocket connection open'
+      cid = rand(36**8).to_s(36) # generate client id
+      puts "#{cid} connected to #{handshake.path}."
+
+      @clients << ws
+
+      ws.send [cid, 'ws', 'connected'].to_json
+
+      send_n_connections(cid)
+    end
+
+    ws.onclose do
+      puts 'Closed.'
+      ws.send ['', 'ws', 'closed'].to_json
+      @clients.delete ws
+      send_n_connections
+    end
+
+    ws.onmessage do |msg|
+      puts "Received: #{msg}"
       @clients.each do |socket|
         socket.send msg
       end
     end
   end
 
+  EM.open_keyboard(KeyboardHandler, @clients)
+
+  puts 'WebSocket server running... press ctrl-c to stop.'
 end
-
-EM.run {
-  @clients = []
-
-  # Update all clients with number of total connections
-  def sendNConnections(cid = nil)
-      puts "#{@clients.length} connections open"
-
-      msg = [cid, "ws", "n", @clients.length].to_json
-
-      @clients.each do |socket|
-        socket.send msg
-      end
-  end
-
-  EM::WebSocket.start(:host => '0.0.0.0', :port => '4001') do |ws|
-    ws.onopen { |handshake|
-      puts "WebSocket connection open"
-      cid = rand(36**8).to_s(36) # generate client id
-      puts "#{cid} connected to #{handshake.path}."
-
-      @clients << ws
-
-      ws.send [cid, "ws", "connected"].to_json
-
-      sendNConnections(cid)
-    }
-
-    ws.onclose {
-      puts "Closed."
-      ws.send ["", "ws", "closed"].to_json
-      @clients.delete ws
-      sendNConnections()
-    }
-
-    ws.onmessage { |msg|
-      puts "Received: #{msg}"
-      @clients.each do |socket|
-        socket.send msg
-      end
-    }
-  end
-
-  EM.open_keyboard(MyKeyboardHandler, @clients)
-
-  puts "WebSocket server running... press ctrl-c to stop."
-}
