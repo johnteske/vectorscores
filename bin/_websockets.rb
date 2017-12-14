fff6a9ad0bdc55e9c8f8001e8f46527b18aa20b4
John Teske <john.teske@gmail.com>
2017-12-13 21:49:20 -0800

    Create WebSocketClients module to handle client list

--- bin/_websockets.rb
+++ bin/_websockets.rb
@@ -7,71 +7,76 @@ require 'json'
 module KeyboardHandler
   include EM::Protocols::LineText2
 
-  def initialize(c)
-    @clients = c
-  end
-
   def receive_line(data)
     return unless data =~ /reload/i
 
     msg = ['', 'ws', 'reload'].to_json
 
+    WebSocketClients.send_each(msg)
+  end
+end
+
+# Manage list of WebSocket clients
+module WebSocketClients
+  @clients = []
+
+  def self.add(ws)
+    @clients << ws
+  end
+
+  def self.remove(ws)
+    @clients.delete ws
+  end
+
+  # Generate a unique id, referred to as the cid
+  # TODO: returns a random (not truly unique) id,
+  # which creates a very low chance of having duplicate ids
+  def self.generate_id
+    rand(36**8).to_s(36)
+  end
+
+  def self.send_each(msg)
     @clients.each do |socket|
       socket.send msg
     end
   end
-end
 
-# Generate a unique id, referred to as the cid
-# TODO: returns a random (not truly unique) id,
-# which creates a very low chance of having duplicate ids
-def generate_client_id
-  rand(36**8).to_s(36)
-end
-
-# rubocop:disable BlockLength
-EM.run do
-  @clients = []
-
-  # Update all clients with number of total connections
-  def send_n_connections(cid = nil)
+  def self.send_n_connections(cid = nil)
     puts "#{@clients.length} connections open"
 
     msg = [cid, 'ws', 'n', @clients.length].to_json
 
-    @clients.each do |socket|
-      socket.send msg
-    end
+    send_each(msg)
   end
+end
 
+EM.run do
   EM::WebSocket.start(host: '0.0.0.0', port: '4001') do |ws|
     ws.onopen do |handshake|
-      cid = generate_client_id
+      cid = WebSocketClients.generate_id
       puts "WebSocket connection opened: #{cid} connected to #{handshake.path}."
 
-      @clients << ws
+      WebSocketClients.add(ws)
 
       ws.send [cid, 'ws', 'connected'].to_json
 
-      send_n_connections(cid)
+      WebSocketClients.send_n_connections(cid)
     end
 
     ws.onclose do
       puts 'WebSocket connection closed.'
       ws.send ['', 'ws', 'closed'].to_json
-      @clients.delete ws
-      send_n_connections
+      WebSocketClients.remove(ws)
+      WebSocketClients.send_n_connections
     end
 
     ws.onmessage do |msg|
       puts "Received: #{msg}"
-      @clients.each do |socket|
-        socket.send msg
-      end
+      WebSocketClients.send_each(msg)
     end
   end
 
-  EM.open_keyboard(KeyboardHandler, @clients)
+  EM.open_keyboard(KeyboardHandler)
 
   puts 'WebSocket server running... press ctrl-c to stop.'
 end
