e8c3a60e0538ca2caefaff9829e249461f309e7b
John Teske <john.teske@gmail.com>
2018-04-19 21:47:44 -0700

    Create VS.createHooks, to be used as a replacement for callbacks

---
+++ assets/js/_hooks.js
@@ -0,0 +1,46 @@
+/**
+ * Factory function to add hooks to internal methods
+ * Currently no method to remove at this time as
+ * hooks are not dynamically added/removed
+ * @param {...string} : keys of hooks
+ */
+VS.createHooks = function() {
+
+    var hooks = {};
+
+    /**
+     * Initialize dictionary
+     */
+    var dictionary = {};
+
+    for (var i = 0; i < arguments.length; i++) {
+        dictionary[arguments[i]] = [];
+    }
+
+    /**
+     * Add hook
+     * @param {string} hook : key of hook to add to
+     * @param {function} fn : function to add to hook
+     */
+    hooks.add = function(hook, fn) {
+        if (dictionary[hook]) {
+            dictionary[hook].push(fn);
+        } else {
+            throw new Error('[hooks#add] ' + hook + ' is not a registered hook');
+        }
+    };
+
+    /**
+     * Call all functions for a given hook
+     * @param {string} hook : key of hook to trigger
+     */
+    hooks.trigger = function(hook) {
+        var fns = dictionary[hook];
+
+        for (var i = 0; i < fns.length; i++) {
+            fns[i]();
+        }
+    };
+
+    return Object.freeze(hooks);
+};
---
+++ assets/js/_tests/hooks.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { test, setupDOM } = require(path.resolve('.', 'bin/js/tape-setup'))
+setupDOM('_site/scores/tutorial/index.html')
+
+const VS = require(path.resolve('.', '_site/assets/js/vectorscores.js'))
+
+test('VS#hooks', t => {
+    const hooks = new VS.createHooks('play')
+
+    const expected = {
+        a: 2,
+        b: 4,
+        c: 6
+    }
+    let actual = {}
+
+    hooks.add('play', () => { actual.a = 2 })
+    hooks.add('play', () => { actual.b = 4 })
+    hooks.add('play', () => { actual.c = 6 })
+
+    hooks.trigger('play')
+
+    t.deepEqual(actual, expected, 'should execute all registered callbacks')
+
+    t.throws(() => {
+        hooks.add('test', () => {})
+    }, 'should throw an error when adding an unregistered hook')
+
+    t.end()
+})
