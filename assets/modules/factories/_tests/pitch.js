6d704f3b09b49e552b59ab676059a369571a73db
John Teske <john.teske@gmail.com>
2018-06-25 08:31:19 -0700

    Consolidate work versions and tests

--- assets/modules/factories/_tests/pitch.js
+++ assets/modules/factories/_tests/pitch.js
@@ -1,17 +1,54 @@
 const path = require('path')
 const { test, getWindowFromFile } = require(path.resolve('.', 'bin/js/tape-setup'))
 
-test('factories.pitch', t => {
-    getWindowFromFile('_site/assets/modules/factories/index.html', window => {
-        const { VS } = window
+let window
 
-        const pitch = VS.factories.pitch()
-            .noteName('g')
-            .octave(5)
-
-        t.equal(pitch.number(), 7, 'should return pitch number of 7, given note name \'g\'')
-        t.equal(pitch.octave(), 5, 'should return octave of 5, given octave of 5')
-
+test('setup', t => {
+    getWindowFromFile('_site/assets/modules/factories/index.html', loadedWindow => {
+        window = loadedWindow
         t.end()
     })
+})
+
+test('factories.pitch', t => {
+    const { VS } = window
+
+    const pitch = VS.factories.pitch()
+        .noteName('g')
+        .octave(5)
+
+    t.equal(pitch.noteName(), 'g', 'should have note name of \'g\', given pitch G5')
+    t.equal(pitch.octave(), 5, 'should have octave of 5, given pitch G5')
+    t.equal(pitch.number(), 7, 'should have pitch number of 7, given pitch G5')
+    t.equal(pitch.pitch(), 67, 'should have pitch of 67, given pitch G5')
+
+    t.end()
+})
+
+test('factories.pitch transposition', t => {
+    const { VS } = window
+
+    const pitch = VS.factories.pitch()
+        .pitch(60)
+
+    t.equal(pitch.pitch(), 60)
+    t.equal(pitch.number(), 0)
+    t.equal(pitch.noteName(), 'c')
+    t.equal(pitch.octave(), 5)
+
+    pitch.transpose(7)
+
+    t.equal(pitch.pitch(), 67)
+    t.equal(pitch.number(), 7)
+    t.equal(pitch.noteName(), 'g')
+    t.equal(pitch.octave(), 5)
+
+    pitch.transpose(-27)
+
+    t.equal(pitch.pitch(), 40)
+    t.equal(pitch.number(), 4)
+    t.equal(pitch.noteName(), 'e')
+    t.equal(pitch.octave(), 3)
+
+    t.end()
 })
--- assets/modules/factories/pitch.js
+++ assets/modules/factories/pitch.js
@@ -1,39 +1,121 @@
+/**
+ * TODO do note names and octaves, etc need to be stored?
+ * or are only number stored and display names are calculated?
+ */
 VS.factories = VS.factories || {};
 
-VS.factories.pitch = function() {
-    var _pitchClass = VS.factories.pitchClass();
+(function() {
+    // Adds pitch class functionality to an object (mutates)
+    function hasPitchClass(obj) {
+        var _noteNameMap = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
 
-    // In scientific pitch notation
-    var octave = 4;
+        // TODO rename integer? (currently only supports integers, not microtones)
+        var number = 0;
 
-    function pitch() {}
+        // TODO rename letter?
+        obj.noteName = function(_) {
+            if (arguments.length) {
+                number = _noteNameMap.indexOf(_);
+                return obj;
+            } else {
+                return _noteNameMap[number];
+            }
+        };
 
-    pitch.noteName = function(_) {
-        if (arguments.length) {
-            _pitchClass.noteName(_);
-            return pitch;
-        } else {
-            return _pitchClass.noteName();
-        }
+        obj.number = function(_) {
+            if (arguments.length) {
+                number = +_;
+                return obj;
+            } else {
+                return number;
+            }
+        };
+    }
+
+    function transpose(pitchClass, semitones) {
+        return VS.mod(+semitones + pitchClass, 12);
+    }
+
+    VS.factories.pitchClass = function() {
+
+        function pitchClass() {}
+
+        hasPitchClass(pitchClass);
+
+        pitchClass.transpose = function(_) {
+            if (arguments.length) {
+                var number = this.number();
+                this.number(transpose(number, _));
+                return this;
+            }
+        };
+
+        return pitchClass;
     };
 
-    pitch.number = function(_) {
-        if (arguments.length) {
-            _pitchClass.number(_);
-            return pitch;
-        } else {
-            return _pitchClass.number();
+    VS.factories.pitch = function() {
+
+        function _pitchClass() {}
+        hasPitchClass(_pitchClass);
+
+        // Scientific pitch notation, C4
+        // "Octave-major order": precisePitch = (Scientific pitch notation octave * 12) + (pitch class number)
+        // C0 = 0
+        var precisePitch = 48;
+
+        function pitch() {}
+
+        pitch.noteName = function(_) {
+            if (arguments.length) {
+                _pitchClass.noteName(_);
+                setPitchWithinOctave();
+                return this;
+            } else {
+                return _pitchClass.noteName();
+            }
+        };
+
+        pitch.number = function(_) {
+            if (arguments.length) {
+                _pitchClass.number(_);
+                setPitchWithinOctave();
+                return this;
+            } else {
+                return _pitchClass.number();
+            }
+        };
+
+        function setPitchWithinOctave() {
+            precisePitch = (pitch.octave() * 12) + pitch.number();
         }
+
+        pitch.pitch = function(_) {
+            if (arguments.length) {
+                precisePitch = +_;
+                return this;
+            } else {
+                return precisePitch;
+            }
+        };
+
+        pitch.transpose = function(_) {
+            if (arguments.length) {
+                precisePitch += +_;
+                _pitchClass.number(VS.mod(precisePitch, 12));
+                return this;
+            }
+        };
+
+        pitch.octave = function(_) {
+            if (arguments.length) {
+                precisePitch = (+_ * 12) + _pitchClass.number();
+                return this;
+            } else {
+                return (precisePitch / 12) >> 0;
+            }
+        };
+
+        return pitch;
     };
 
-    pitch.octave = function(_) {
-        if (arguments.length) {
-            octave = +_;
-            return pitch;
-        } else {
-            return octave;
-        }
-    };
-
-    return pitch;
-}
+})();
