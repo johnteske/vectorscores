8064d471ffe0d8618259d9793eddbd4f47459c69
John Teske <john.teske@gmail.com>
2018-07-03 22:38:16 -0700

    Refactor tutorial to use internal score over VS.score#functionAt

--- scores/tutorial/index.js
+++ scores/tutorial/index.js
@@ -4,8 +4,8 @@ layout: compress-js
 
 d3.select('svg').remove();
 
-var message = document.getElementsByClassName('tutorial-message')[0];
-var initialMessage = message.innerHTML;
+var messageElement = document.getElementsByClassName('tutorial-message')[0];
+var initialMessage = messageElement.innerHTML;
 
 var scoreButtons = document.querySelectorAll('.score-button');
 var footerInput = document.querySelector('#score-footer input');
@@ -23,79 +23,84 @@ function highlightByID(id) {
     document.getElementById(id).classList.add('highlight');
 }
 
+function describeControl(ids, message) {
+    clearHighlights();
+    ids.forEach(highlightByID);
+    messageElement.innerHTML = message;
+}
+
 /**
  * Create events
  */
-var addEvent = (function() {
-    var time = 0;
+var score = [
+    {
+        ids: [],
+        message: initialMessage,
+        duration: 3
+    },
+    {
+        ids: [],
+        message: 'The header and footer of a score will disappear when playing and will reappear when paused or stopped.',
+        duration: 5
+    },
+    {
+        ids: ['score-play'],
+        message: 'To start or pause score play, click the play/pause button or press the spacebar.',
+        duration: 3
+    },
+    {
+        ids: ['score-stop'],
+        message: 'To stop score play, click the stop button or press the escape key.',
+        duration: 3
+    },
+    {
+        ids: ['score-pointer'],
+        message: 'The current score position is displayed here.',
+        duration: 3
+    },
+    {
+        ids: ['score-back', 'score-fwd'],
+        message: 'When a score is not playing, you can set the score position with the forward and back buttons or by using the left and right arrow keys.',
+        duration: 3
+    },
+    {
+        ids: ['score-info-open'],
+        message: 'If a score has additional information such as performance notes, they can be displayed by clicking the info button.',
+        duration: 3
+    },
+    {
+        ids: ['score-options-open'],
+        message: 'If a score has options that can be set by performers, they can accessed by clicking the options button.',
+        duration: 3
+    },
+    {
+        ids: [],
+        message: '',
+        duration: 1
+    }
+].map(addTime);
 
-    return function(fn, duration) {
-        VS.score.add(time, fn);
-        time += duration;
-    };
-})();
+function addTime(bar, i, score) {
+    bar.time = score.slice(0, i).reduce(function(sum, bar2) {
+        return sum += bar2.duration;
+    }, 0) * 1000;
 
-addEvent(function() {
-    clearHighlights();
-    message.innerHTML = initialMessage;
-}, 3000);
+    return bar;
+}
 
-addEvent(function() {
-    clearHighlights();
-    message.innerHTML = 'The header and footer of a score will disappear when playing and will reappear when paused or stopped.';
-    // VS.score.schedule(1, VS.score.pause);
-}, 5000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-play');
-    message.innerHTML = 'To start or pause score play, click the play/pause button or press the spacebar.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-stop');
-    message.innerHTML = 'To stop score play, click the stop button or press the escape key.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-pointer');
-    message.innerHTML = 'The current score position is displayed here.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-back');
-    highlightByID('score-fwd');
-    message.innerHTML = 'When a score is not playing, you can set the score position with the forward and back buttons or by using the left and right arrow keys.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-info-open');
-    message.innerHTML = 'If a score has additional information such as performance notes, they can be displayed by clicking the info button.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    highlightByID('score-options-open');
-    message.innerHTML = 'If a score has options that can be set by performers, they can accessed by clicking the options button.';
-}, 3000);
-
-addEvent(function() {
-    clearHighlights();
-    message.innerHTML = '';
-}, 3000);
+score.forEach(function(bar) {
+    VS.score.add(bar.time, describeControl, [bar.ids, bar.message])
+})
 
 /**
- *
+ * Controls
  */
 VS.control.hooks.add('step', function() {
-    VS.score.functionAt(VS.score.getPointer())();
+    var bar = score[VS.score.getPointer()];
+    describeControl(bar.ids, bar.message);
 });
 
 VS.score.hooks.add('stop', function() {
-   message.innerHTML = initialMessage;
-   clearHighlights();
+    messageElement.innerHTML = initialMessage;
+    clearHighlights();
 });
