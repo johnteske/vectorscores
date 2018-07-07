055af1719a300650652021db8f26151b3a682879
John Teske <john.teske@gmail.com>
2018-07-07 08:50:47 -0700

    Clean up spike

--- scores/trashfire/_spike.js
+++ scores/trashfire/_spike.js
@@ -1,18 +1,16 @@
-TrashFire.spike = (function() {
+TrashFire.spike = (function(tf) {
     var spike = {};
 
-    var x = TrashFire.view.width * 0.5;
+    var x = tf.view.width * 0.5;
 
-    spike.group = TrashFire.wrapper.append('g')
-        .attr('class', 'spike')
+    var path = tf.wrapper
+        .append('path')
+        .attr('d', 'M-15,0 L15,0 L0,60 Z')
         .style('opacity', 0);
 
-    spike.group.append('path')
-        .attr('d', 'M-15,0 L15,0 L0,60 Z');
-
     spike.show = function() {
-        spike.group
-            .attr('transform', 'translate(' + x + ','  + 15 + ')')
+        path
+            .attr('transform', translateY(15))
             .style('opacity', 0)
             .transition().duration(600)
             .style('opacity', 1);
@@ -22,14 +20,20 @@ TrashFire.spike = (function() {
         trash.empty();
         trash.update(300);
 
-        d3.select('.spike')
+        path
             .transition().duration(600).ease(d3.easeElastic)
-            .attr('transform', 'translate(' + x + ','  + (TrashFire.dumpster.y - 45) + ')')
+            .attr('transform', translateY(tf.dumpster.y - 45))
             .transition().duration(150).ease(d3.easeLinear)
             .style('opacity', 0);
 
         dumpster.shake();
     };
 
+    function translateY(y) {
+        return function() {
+            return 'translate(' + x + ', ' + y + ')';
+        };
+    }
+
     return spike;
-})();
+})(TrashFire);
