7209cfe6babec257322efa524a26edb982034adc
John Teske <john.teske@gmail.com>
2018-07-23 11:10:28 -0700

    Use map in createScoreFragment

--- scores/topographies-ii/walk-reveal/_score.js
+++ scores/topographies-ii/walk-reveal/_score.js
@@ -12,15 +12,11 @@ function getScoreRange(data) {
  * Assign properties to row-major order data
  */
 function createScoreFragment(data) {
-    for (var i = 0; i < data.length; i++) {
-        var h = data[i];
-
-        data[i] = {
-            height: h,
-            heightIndex: ~~h, // h > 0 ? Math.floor(h) : Math.ceil(h)
+    return data.map(function(d) {
+        return {
+            height: d,
+            heightIndex: ~~d,
             revealed: 0
         };
-    }
-
-    return data;
+    });
 }
