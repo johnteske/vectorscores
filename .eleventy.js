7d107f2d6198d90de9e924c0e8b8669e1dd6e308
John Teske <john.teske@gmail.com>
2018-12-13 21:44:26 -0800

    Move and alias layouts and includes

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Aliases are in relation to the _includes folder
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
+  eleventyConfig.addLayoutAlias('compress', 'layouts/compress.html');
+  eleventyConfig.addLayoutAlias('compress-js', 'layouts/compress-js.html');
+  
+  eleventyConfig.addLayoutAlias('score', 'layouts/score.html');
+  eleventyConfig.addLayoutAlias('score-set', 'layouts/score-set.html');
+  eleventyConfig.addLayoutAlias('movement', 'layouts/movement.html');
+  return {
+    dir: {
+      input: "./",
+      output: "./_site"
+    }
+  };
+}
