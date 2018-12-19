e449e00ce5cf79299b7c08fba133aaa4b00bb898
John Teske <john.teske@gmail.com>
2018-12-18 20:07:11 -0800

    Use njk as templating engine (no tags changed)

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,21 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setLiquidOptions({
+module.exports = function(config) {
+  config.setLiquidOptions({
     dynamicPartials: true
   });
 
-  eleventyConfig.addPassthroughCopy("assets");
+  config.markdownTemplateEngine = "njk";
+
+  config.addPassthroughCopy("assets");
 
   // Aliases are in relation to the _includes folder
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-  eleventyConfig.addLayoutAlias('compress', 'layouts/compress.html');
-  eleventyConfig.addLayoutAlias('compress-js', 'layouts/compress-js.html');
+  config.addLayoutAlias('default', 'layouts/default.html');
+  config.addLayoutAlias('page', 'layouts/page.html');
+  config.addLayoutAlias('compress', 'layouts/compress.html');
+  config.addLayoutAlias('compress-js', 'layouts/compress-js.html');
   
-  eleventyConfig.addLayoutAlias('score', 'layouts/score.html');
-  eleventyConfig.addLayoutAlias('score-set', 'layouts/score-set.html');
-  eleventyConfig.addLayoutAlias('movement', 'layouts/movement.html');
+  config.addLayoutAlias('score', 'layouts/score.html');
+  config.addLayoutAlias('score-set', 'layouts/score-set.html');
+  config.addLayoutAlias('movement', 'layouts/movement.html');
 
   return {
     dir: {
