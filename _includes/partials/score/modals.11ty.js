4617bed707b2b66163fdb1ff4bbcb1b287135a12
John Teske <john.teske@gmail.com>
2019-02-17 18:46:48 -0800

    Write maybe and maybeTemplate helpers for modal partial

--- _includes/partials/score/modals.11ty.js
+++ _includes/partials/score/modals.11ty.js
@@ -1,5 +1,5 @@
-const rootRequire = require('app-root-path').require;
-const { handleUndefined } = rootRequire('render-utils')
+const maybe = (value) => value ? value : ''
+const maybeTemplate = (template, value) => value ? template : ''
 
 const infoModal = data => 
     `<div id="score-info-modal" class="modal-content">
@@ -7,20 +7,21 @@ const infoModal = data =>
         <h2 class="score-title">${data.title}</h2>
         <p>
             ${data.composer}
-            ${handleUndefined(data.publish_date)}
+            ${maybe(data.publish_date)}
         </p>
-	${data.instrumentation ? '<p>for ${data.instrumentation}</p>' : ''}
-        <p>${handleUndefined(data.dedication)}</p>
-        ${data.info ? `<div>${data.info}</div>` : ''}
-    </div>`; 
+	${maybeTemplate(`<p>for ${data.instrumentation}</p>`, data.instrumentation)}
+        <p>${maybe(data.dedication)}</p>
+	${maybeTemplate(`<div>${data.info}</div>`, data.info)}
+    </div>`;
 
-const optionsModal = data => data.options ?
-    `<div id="score-options-modal" class="modal-content">
-        <span id="score-options-close" class="modal-close">×</span>
-        <h2>Options</h2>
-        ${data.options ? `<div>${data.options}</div>` : ''}
-    </div>` :
-    '';
+const optionsModal = data =>
+    maybeTemplate(
+        `<div id="score-options-modal" class="modal-content">
+            <span id="score-options-close" class="modal-close">×</span>
+            <h2>Options</h2>
+            <div>${data.options}</div>
+        </div>`,
+    data.options)
 
 module.exports = data =>
     `<div id="score-modal-overlay" class="modal-overlay">
