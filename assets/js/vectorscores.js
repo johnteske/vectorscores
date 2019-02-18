dcfc44e3fd2f9ad3be16578fa08ab248d9689d91
John Teske <john.teske@gmail.com>
2019-02-17 21:28:25 -0800

    Add vectorscores script, as empty object

--- assets/js/vectorscores.js
+++ assets/js/vectorscores.js
@@ -1,14 +1,12 @@
----
-layout: compress-js
----
+const VS = {};
 
-var VS = {};
-
-{% include_relative _helpers.util.js %}
-{% include_relative _helpers.math.js %}
-{% include_relative _hooks.js %}
-{% include_relative _layout.js %}
-{% include_relative _score.js %}
-{% include_relative _score.options.js %}
-{% include_relative _controls.js %}
-{% include_relative _modals.js %}
+/*
+require('helpers.util')
+require('helpers.math')
+require('hooks')
+require('layout')
+require('score')
+require('score.options')
+require('controls')
+require('modals')
+*/
