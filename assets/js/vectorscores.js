b870a1f10373c0a4904211cdc915523e259eeb80
John Teske <john.teske@gmail.com>
2019-02-18 18:55:28 -0800

    Add score options

--- assets/js/vectorscores.js
+++ assets/js/vectorscores.js
@@ -3,17 +3,18 @@ import * as math from './vs/helpers.math.js';
 import * as hooks from './vs/hooks.js';
 import * as layout from './vs/layout.js';
 import * as score from './vs/score.js';
+import * as scoreOptions from './vs/score-options.js';
 
 window.VS = {
     ...util,
     ...math,
     ...hooks,
     ...layout,
-    ...score
+    ...score,
+    ...scoreOptions
 };
 
 /*
-require('score.options')
 require('controls')
 require('modals')
 */
