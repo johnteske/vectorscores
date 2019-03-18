3773a00e0ce2d1b8548e55358a3916e76fed6aec
John Teske <john.teske@gmail.com>
2019-03-17 21:27:35 -0700

    Commit 3d-object template

---
+++ scores/3d-object/index.11ty.js
@@ -0,0 +1,13 @@
+module.exports = class {
+    data() {
+        return {
+	    layout: 'score',
+	    title: '3D object',
+	    status: 'unlisted'
+	}
+    }
+
+    render(data) {
+        return ``;
+    }
+};
