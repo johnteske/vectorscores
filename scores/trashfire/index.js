8a7cfa13171b03e8177a9a077c97f36b333dc118
John Teske <john.teske@gmail.com>
2019-04-28 10:42:15 -0700

    Add resize script

--- scores/trashfire/index.js
+++ scores/trashfire/index.js
@@ -1,28 +1,61 @@
-import { TrashFire, layout } from "./_setup"
+import { TrashFire, layout } from "./_setup";
 
-import * as utils from "./_utils"
+import * as utils from "./_utils";
 
-import makeDumpster from "./_dumpster.js"
-const dumpster = makeDumpster(TrashFire)
+import makeDumpster from "./_dumpster.js";
+const dumpster = makeDumpster(TrashFire);
 
-import makeTrash from "./_trash.js"
-const trash = makeTrash(TrashFire)
-import makeSpike from "./_spike.js"
-const spike = makeSpike(TrashFire)
-import makeNoise from "./_noise.js"
-const noise = makeNoise(TrashFire)
-import makeScrapeDrone from "./_scrape-drone.js"
-const scrapeDrone = makeScrapeDrone(TrashFire)
+import makeTrash from "./_trash.js";
+const trash = makeTrash(TrashFire);
+import makeSpike from "./_spike.js";
+const spike = makeSpike(TrashFire);
+import makeNoise from "./_noise.js";
+const noise = makeNoise(TrashFire);
+import makeScrapeDrone from "./_scrape-drone.js";
+const scrapeDrone = makeScrapeDrone(TrashFire);
 
-import score from "./score/index.js"
+import score from "./score/index.js";
 score.forEach(function(bar) {
-    VS.score.add(bar.time, bar.fn, bar.args);
+  VS.score.add(bar.time, bar.fn, bar.args);
 });
+
 //import controls from "./_controls.js"
-//import resize from "./_resize.js"
+
+/**
+ * Resize
+ */
+function resize() {
+  var main = layout.main;
+
+  var w = (layout.main.width = parseInt(main.style("width"), 10));
+  var h = (layout.main.height = parseInt(main.style("height"), 10));
+
+  var scaleX = VS.clamp(w / layout.width, 0.25, 2);
+  var scaleY = VS.clamp(h / layout.height, 0.25, 2);
+
+  layout.scale = Math.min(scaleX, scaleY);
+
+  layout.margin.left = w * 0.5 - layout.width * 0.5 * layout.scale;
+  layout.margin.top = h * 0.5 - layout.height * 0.5 * layout.scale;
+
+  TrashFire.wrapper.attr(
+    "transform",
+    "translate(" +
+      layout.margin.left +
+      "," +
+      layout.margin.top +
+      ") scale(" +
+      layout.scale +
+      "," +
+      layout.scale +
+      ")"
+  );
+}
+
+d3.select(window).on("resize", resize);
 
 // TODO implement
-d3.select(window).on('load', function() {
-  // resize();
+d3.select(window).on("load", function() {
+  resize();
   // TrashFire.noiseLayer.render();
 });
