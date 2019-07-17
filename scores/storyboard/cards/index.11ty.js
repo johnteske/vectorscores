ba1be2b242f3d02722044f2a9388df714f413785
John Teske <john.teske@gmail.com>
2019-07-17 16:09:33 -0700

    Convert storyboard cards

---
+++ scores/storyboard/cards/index.11ty.js
@@ -0,0 +1,21 @@
+const options = require("./_options.11ty.js");
+
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "cards",
+      status: "wip",
+      options: options(),
+      modules: [
+        "cue/blink",
+        "dictionary",
+        "bravura",
+        "pitch-class",
+        "settings/radio"
+      ]
+    }
+  }
+
+  render(data) { return ``; }
+}
--- scores/storyboard/cards/index.js
+++ scores/storyboard/cards/index.js
@@ -1,11 +1,89 @@
----
-layout: compress-js
----
-
 var cardTypes, cardList;
 
-{% include_relative _score.js %}
-{% include_relative _settings.js %}
+cardTypes = {
+    'A': {
+        numberOfNotes: [1, 5], // low limit, high limit
+        dynamics: ['pp', 'p', 'mp'],
+        spread: {x: 0, y: 0}, // single note
+        pcSet: [0, 1, 3]
+    },
+    'B': {
+        numberOfNotes: [1, 10],
+        dynamics: ['p', 'mp'],
+        spread: {x: 0, y: 25}, // chord, cluster
+        pcSet: [0, 1, 4]
+    },
+    'C': {
+        numberOfNotes: [6, 15],
+        dynamics: ['mp', 'mf'],
+        spread: {x: 50, y: 0}, // rhythm
+        pcSet: [0, 1, 5]
+    },
+    'D': {
+        numberOfNotes: [11, 20],
+        dynamics: ['mf', 'f'],
+        spread: {x: 50, y: 25}, // rect cloud
+        pcSet: [0, 2, 5]
+    },
+    'E': {
+        numberOfNotes: [16, 25],
+        dynamics: ['f', 'ff', 'fff'],
+        spread: {x: 35, y: 35}, // cloud
+        pcSet: [0, 2, 6]
+    }
+};
+
+function makeCards(nCards) {
+    var _cards = [],
+        _cardTypeKeys = ['A', 'B', 'C', 'D', 'E']; // Object.keys(cardTypes);
+
+    for (var i = 0; i < nCards; i++) {
+        var thisCard = {},
+            thisCardType,
+            prevCard = _cards[i - 1] || { time: 0 };
+
+        // set unique type
+        do {
+            thisCard.type = VS.getItem(_cardTypeKeys);
+        } while (thisCard.type === prevCard.type);
+
+        thisCardType = cardTypes[thisCard.type];
+
+        // set card start time
+        thisCard.time = prevCard.time + VS.getRandExcl(5000, 8000);
+        // thisCard.duration // could also/instead set duration for each card
+
+        thisCard.nnotes = Math.floor(VS.getRandExcl(
+            thisCardType.numberOfNotes[0], thisCardType.numberOfNotes[1]
+        ));
+
+        thisCard.spread = thisCardType.spread;
+
+        thisCard.dynamic = VS.getItem(thisCardType.dynamics);
+
+        thisCard.pcSet = VS.pitchClass.transpose(thisCardType.pcSet, 'random');
+
+        _cards.push(thisCard); // console.log(thisCard);
+    }
+    return _cards;
+}
+
+cardList = makeCards(12);
+var scoreSettings = (function() {
+    var generateButton = document.getElementById('settings-generate'),
+        radioSetting = new VS.RadioSetting(document.getElementsByName('settings-pc-display'));
+
+    var settings = {};
+
+    settings.pcFormat = VS.getQueryString('pcs') || '';
+    radioSetting.set(settings.pcFormat);
+
+    generateButton.addEventListener('click', function() {
+        document.location.href = '?pcs=' + radioSetting.get();
+    });
+
+    return settings;
+})();
 
 var dynamicsDict = VS.dictionary.Bravura.dynamics,
     durationDict = VS.dictionary.Bravura.durations.stemless;
