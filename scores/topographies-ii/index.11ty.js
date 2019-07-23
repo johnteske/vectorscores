ebd9c23f9d58d27eed36d1f86a9aedf66933aa43
John Teske <john.teske@gmail.com>
2019-07-22 19:59:45 -0700

    Convert topographies

---
+++ scores/topographies-ii/index.11ty.js
@@ -0,0 +1,15 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "score-set",
+      title: "topographies, book ii",
+      instrumentation: "any ensemble",
+      status: "wip",
+      formats: ["score"]  
+    }
+  }
+
+  render(data) {
+    return ``;
+  }
+}
---
+++ scores/topographies-ii/walk-reveal/index.11ty.js
@@ -0,0 +1,26 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "walk reveal",
+      status: "wip",
+      info: `
+        <p>
+          Travel through the landscape, interpreting small areas of symbols as musical phrases and textures.
+          The symbols should not be interpreted one at a time nor should they be taken literally.
+        </p>
+        <p>
+          You may choose to rest at times during your travels to allow room for variety in instrumentation and texture.
+        </p>
+      `,
+      modules: [
+        "dictionary",
+        "bravura"
+      ]
+    }
+  }
+
+  render(data) {
+    return ``;
+  }
+}
