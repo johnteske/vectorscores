6a532407d77ed175afde18b057438dac8f21965c
John Teske <john.teske@gmail.com>
2019-08-10 15:53:50 -0700

    Move drone renderer to 'thread' for use in all mvt

--- scores/threads/death-rattle/index.js
+++ scores/threads/death-rattle/index.js
@@ -1,16 +1,7 @@
+import drone from "../drone"
+// const drone = require("../drone")
+
 const main = d3.select(".main");
 const wrapper = main.append("g");
 
-// initial drone, to become module for this suite
-const data = [ {x:0, y:50}, {x:0, y:100} ]
-
-wrapper
-  .selectAll(".drone")
-  .data(data)
-  .enter()
-  .append("line")
-  .attr("class", "drone")
-  .attr("x1", 0)
-  .attr("x2", 100)
-  .attr("y1", d => d.y)
-  .attr("y2", d => d.y);
+drone(wrapper)
---
+++ scores/threads/drone.js
@@ -0,0 +1,14 @@
+const data = [ {x:0, y:50}, {x:0, y:100} ]
+
+export default function(selection) {
+  selection
+    .selectAll(".drone")
+    .data(data)
+    .enter()
+    .append("line")
+    .attr("class", "drone")
+    .attr("x1", 0)
+    .attr("x2", 100)
+    .attr("y1", d => d.y)
+    .attr("y2", d => d.y);
+}
