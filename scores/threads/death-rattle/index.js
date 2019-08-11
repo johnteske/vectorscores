0b978c4c540953165aba7f4c947e6196c20ca2e3
John Teske <john.teske@gmail.com>
2019-08-11 14:28:50 -0700

    Add basic "shiver"

--- scores/threads/death-rattle/index.js
+++ scores/threads/death-rattle/index.js
@@ -4,3 +4,25 @@ const main = d3.select(".main");
 const wrapper = main.append("g");
 
 drone(wrapper);
+
+// shiver--or shudder?
+function shiver(selection) {
+  const group = selection.append("g").attr("transform", "translate(50,50)");
+
+  group
+    .append("text")
+    .text("cres.")
+    .attr("x", 0);
+
+  group
+    .append("text")
+    .text("///")
+    .attr("x", 50);
+
+  group
+    .append("text")
+    .text("decres.")
+    .attr("x", 100);
+}
+
+shiver(wrapper);
