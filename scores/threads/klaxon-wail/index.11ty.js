30b3ff39747e93d990872c4745770a66b6d5840d
John Teske <john.teske@gmail.com>
2019-08-17 10:48:35 -0700

    Add klaxon wail

---
+++ scores/threads/klaxon-wail/index.11ty.js
@@ -0,0 +1,15 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "klaxon wail",
+      composer: "John Teske",
+      status: "wip",
+      modules: ["bravura"]
+    };
+  }
+
+  render(data) {
+    return ``;
+  }
+};
---
+++ scores/threads/klaxon-wail/index.js
@@ -0,0 +1,11 @@
+import drone from "../drone";
+
+const main = d3.select(".main");
+const wrapper = main.append("g");
+
+drone(wrapper);
+
+function wail() {
+  // long, periodic
+  // accented, falling off
+}
