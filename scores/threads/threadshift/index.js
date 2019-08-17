fd13957f22e14846bf7b8744b846003207eb25e9
John Teske <john.teske@gmail.com>
2019-08-17 14:02:14 -0700

    Add split bar, basic shape

--- scores/threads/threadshift/index.js
+++ scores/threads/threadshift/index.js
@@ -3,7 +3,7 @@ import drone from "../drone";
 const main = d3.select(".main");
 const wrapper = main.append("g");
 
-// drone(wrapper);
+// drone(wrapper); // TODO
 
 var durations = VS.dictionary.Bravura.durations.stemless;
 
@@ -31,21 +31,47 @@ const score = [
     duration: null,
     render: ({ startTime, duration }) => {
       const g = longTone(wrapper, startTime, 50, duration);
-      g.append("text").text(">");
-      g.append("text").text("p, cres., mf");
+      g.append("text").text(">").attr("dy", "1em");
+      g.append("text").text("p, cres., mf").attr("dy", "2em");
     }
   },
   {
     startTime: null,
     duration: null,
-    render: ({ startTime }) => {
-      wrapper
-        .append("text")
-        .text("cluster //")
+    render: ({ startTime, duration }) => {
+      const g = wrapper
+        .append("g")
         .attr("transform", `translate(${startTime},50)`);
+
+      g.append("text").text("cluster");
+      g.append("text").text("//").attr("x", duration).attr("dx", "-2em");
+    }
+  },
+  {
+    startTime: null,
+    duration: null,
+    render: ({ startTime, duration }) => {
+      const g = wrapper
+        .append("g")
+        .attr("transform", `translate(${startTime},50)`);
+
+      // top line
+      g.append("line")
+        .attr("x1", 0)
+        .attr("x2", duration);
+      g.append("text").text("sfz, decres. to niente")
+        .attr("dy", "-1em")
+
+      // bottom line
+      g.append("line")
+        .attr("x1", 0)
+        .attr("x2", duration)
+        .attr("y2", 50); // TODO curve and draw out, for more beating
+      g.append("text").text("sfz, mf, decres. to p")
+        .attr("y", 50)
+        .attr("dy", "1em")
     }
   }
-  // solo part drops below and ensemble plays ghost of former long tone
   // threads are revealed by ensemble as the solo line becomes stable
 ].map((bar, i) => {
   // TODO each bar is saet to the same duration during sketching
