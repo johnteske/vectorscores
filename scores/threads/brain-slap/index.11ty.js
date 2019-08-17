4f56160e912bd7ae7d89f036714e1e6a5c928ce8
John Teske <john.teske@gmail.com>
2019-08-17 10:48:23 -0700

    Add brain slap

---
+++ scores/threads/brain-slap/index.11ty.js
@@ -0,0 +1,15 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "brain slap",
+      composer: "John Teske",
+      status: "wip",
+      modules: ["bravura"]
+    };
+  }
+
+  render(data) {
+    return ``;
+  }
+};
---
+++ scores/threads/brain-slap/index.js
@@ -0,0 +1,15 @@
+import drone from "../drone";
+
+const main = d3.select(".main");
+const wrapper = main.append("g");
+
+drone(wrapper);
+
+// wet slaps:
+// tongue slap
+// col legno battuto
+
+// sloshing:
+// bow hair rotating?
+
+// after slaps, drones are more meandering?
