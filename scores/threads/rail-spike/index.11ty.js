e15afedadc4dcec5323a52e94b95ab8ba7ce50f3
John Teske <john.teske@gmail.com>
2019-08-17 09:58:09 -0700

    Add rail spike

---
+++ scores/threads/rail-spike/index.11ty.js
@@ -0,0 +1,17 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "rail spike",
+      composer: "John Teske",
+      status: "wip",
+      modules: [
+        "bravura"
+      ]
+    };
+  }
+
+  render(data) {
+    return ``;
+  }
+};
---
+++ scores/threads/rail-spike/index.js
@@ -0,0 +1,8 @@
+import drone from "../drone";
+
+const main = d3.select(".main");
+const wrapper = main.append("g");
+
+drone(wrapper);
+
+// TODO import spike from trashfire?
