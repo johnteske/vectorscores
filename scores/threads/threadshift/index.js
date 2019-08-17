38494b49282ae57e926db3b9aaa7c9e0038a2cc6
John Teske <john.teske@gmail.com>
2019-08-17 13:41:01 -0700

    Add first bars with rendering functions

--- scores/threads/threadshift/index.js
+++ scores/threads/threadshift/index.js
@@ -7,10 +7,10 @@ const wrapper = main.append("g");
 
 var durations = VS.dictionary.Bravura.durations.stemless;
 
-function longTone(selection) {
+function longTone(selection, x, y, duration) {
   const group = selection.append("g");
 
-  group.attr("transform", "translate(0, 50)")
+  group.attr("transform", `translate(${x}, ${y})`);
 
   group
     .append("text")
@@ -20,16 +20,40 @@ function longTone(selection) {
   group
     .append("line")
     .attr("x1", "0.5em")
-    .attr("x2", 50)
+    .attr("x2", x + duration);
+
+  return group;
 }
 
-longTone(wrapper);
-
 const score = [
-  // solo long tone (p), with accent
-  // crescendos to mf
-  // spike
-  // railroad tracks
+  {
+    startTime: null,
+    duration: null,
+    render: ({ startTime, duration }) => {
+      const g = longTone(wrapper, startTime, 50, duration);
+      g.append("text").text(">");
+      g.append("text").text("p, cres., mf");
+    }
+  },
+  {
+    startTime: null,
+    duration: null,
+    render: ({ startTime }) => {
+      wrapper
+        .append("text")
+        .text("cluster //")
+        .attr("transform", `translate(${startTime},50)`);
+    }
+  }
   // solo part drops below and ensemble plays ghost of former long tone
   // threads are revealed by ensemble as the solo line becomes stable
-];
+].map((bar, i) => {
+  // TODO each bar is saet to the same duration during sketching
+  const length = 150;
+  return { ...bar, duration: length, startTime: length * i };
+});
+
+score.forEach(bar => {
+  const { render, ...barData } = bar;
+  render(barData);
+});
