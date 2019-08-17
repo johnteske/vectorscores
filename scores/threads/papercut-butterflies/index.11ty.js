b96becfdbdcc54fadce04ea495d3398e14b87212
John Teske <john.teske@gmail.com>
2019-08-17 10:07:53 -0700

    Add papercut butterflies

---
+++ scores/threads/papercut-butterflies/index.11ty.js
@@ -0,0 +1,15 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "papercut butterflies",
+      composer: "John Teske",
+      status: "wip",
+      modules: ["bravura"]
+    };
+  }
+
+  render(data) {
+    return ``;
+  }
+};
---
+++ scores/threads/papercut-butterflies/index.js
@@ -0,0 +1,22 @@
+import drone from "../drone";
+
+const main = d3.select(".main");
+const wrapper = main.append("g");
+
+drone(wrapper);
+
+// musicians move from supporting texture to attacks and return
+// control the density and rate of this balance of the piece
+
+function supportingTexture() {
+  // an open fourth, fifth, or sixth
+}
+
+function exhale() {
+  // white noise, decres., mimicking a flap of the wings
+}
+function attack() {
+  // one to three times each phrase
+  // metal scrape: sfz gliss up
+  // other scrapes: nients cres., gliss up, end with accented release
+}
