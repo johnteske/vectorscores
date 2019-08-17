c08e96fbf670df2b96e259a9a7f3fb3ed8066090
John Teske <john.teske@gmail.com>
2019-08-17 10:30:52 -0700

    Add "by the neck"

---
+++ scores/threads/by-the-neck/index.11ty.js
@@ -0,0 +1,15 @@
+module.exports = class {
+  data() {
+    return {
+      layout: "movement",
+      title: "by the neck",
+      composer: "John Teske",
+      status: "wip",
+      modules: ["bravura"]
+    };
+  }
+
+  render(data) {
+    return ``;
+  }
+};
---
+++ scores/threads/by-the-neck/index.js
@@ -0,0 +1,30 @@
+import drone from "../drone";
+
+const main = d3.select(".main");
+const wrapper = main.append("g");
+
+drone(wrapper);
+
+// what does it sound like when
+// Satan grabs the back of your neck
+// with his right hand, with nails, and
+// leans over your left shoulder?
+// you can feel his breath in your
+// ear, never words. never seeing,
+// only feeling
+
+function lowDrone() {
+  // LNP, trem.
+}
+
+function growl() {
+  // exponential cres.
+}
+
+const score = [
+  // start with intense breath sounds (or maybe they come in later?)
+  // add low scrape
+  // add low drone
+  // scrape and drone cres., more pressure
+  // wall of texture, frantic, growling
+];
