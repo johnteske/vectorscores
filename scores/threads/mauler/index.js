dcee56b48a4e46daf1f27a4383a62b6a483e9e58
John Teske <john.teske@gmail.com>
2019-09-07 17:31:35 -0700

    Edit sixteenths gesture

--- scores/threads/mauler/index.js
+++ scores/threads/mauler/index.js
@@ -1,42 +1,50 @@
 import drone from "../drone";
+import translate from "../translate";
 
 const durations = VS.dictionary.Bravura.durations.stemless;
 
 const main = d3.select(".main");
 const wrapper = main.append("g");
 
-drone(wrapper);
+//drone(wrapper);
 
 function sixteenths(selection) {
   const g = selection.append("g").attr("stroke", "black");
 
-  [0, 1, 2, 3, 4].forEach(x => {
+  const spacing = 5;
+
+  [0, -1, 0, 2, 3].forEach((y, i) => {
+    const x = i * spacing;
+
     g.append("line")
-      .attr("x1", x * 5)
-      .attr("x2", x * 5)
-      .attr("y1", 0)
-      .attr("y2", 10);
+      .attr("x1", x)
+      .attr("x2", x)
+      .attr("y1", 0 - y)
+      .attr("y2", 10 - y);
   });
 
   g.append("line")
     .attr("x1", 0)
-    .attr("x2", 3 * 5)
+    .attr("x2", 3 * spacing)
     .attr("y1", 0)
-    .attr("y2", 0)
+    .attr("y2", 0 - 2)
     .attr("stroke-width", 2);
 
   g.append("line")
     .attr("x1", 0)
-    .attr("x2", 3 * 5)
+    .attr("x2", 3 * spacing)
+    .attr("y1", 0)
     .attr("y1", 3)
-    .attr("y2", 3)
+    .attr("y2", 3 - 2)
     .attr("stroke-width", 2);
 
   return g;
 }
 
 // TODO
-sixteenths(wrapper);
+sixteenths(wrapper).attr("transform", "translate(0, 50) scale(2,2)");
+
+translate(100, 50, sixteenths(wrapper));
 
 const score = [
   // existing drone/s?
