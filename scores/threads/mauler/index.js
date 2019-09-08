7a8dafab6dfa355ac4bc6fbc54bb743ec6e83c85
John Teske <john.teske@gmail.com>
2019-09-07 17:03:23 -0700

    Replace placeholder gesture

--- scores/threads/mauler/index.js
+++ scores/threads/mauler/index.js
@@ -7,14 +7,36 @@ const wrapper = main.append("g");
 
 drone(wrapper);
 
+function sixteenths(selection) {
+  const g = selection.append("g").attr("stroke", "black");
+
+  [0, 1, 2, 3, 4].forEach(x => {
+    g.append("line")
+      .attr("x1", x * 5)
+      .attr("x2", x * 5)
+      .attr("y1", 0)
+      .attr("y2", 10);
+  });
+
+  g.append("line")
+    .attr("x1", 0)
+    .attr("x2", 3 * 5)
+    .attr("y1", 0)
+    .attr("y2", 0)
+    .attr("stroke-width", 2);
+
+  g.append("line")
+    .attr("x1", 0)
+    .attr("x2", 3 * 5)
+    .attr("y1", 3)
+    .attr("y2", 3)
+    .attr("stroke-width", 2);
+
+  return g;
+}
+
 // TODO
-[0, 1, 2, 3, 4].forEach(x => {
-  wrapper
-    .append("text")
-    .style("font-family", "'Bravura'")
-    .text(durations[0.25])
-    .attr("dx", `${x}em`);
-});
+sixteenths(wrapper);
 
 const score = [
   // existing drone/s?
