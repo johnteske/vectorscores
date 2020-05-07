73c3f79e07d4447b8a4200c39a31da6da674765a
John Teske <john.teske@protonmail.com>
2020-05-06 20:50:09 -0700

    Replace ./bin/deploy with gh-pages package

--- _data/site.js
+++ _data/site.js
@@ -2,15 +2,15 @@ module.exports = {
   "title": "vectorscores",
   "email": "john@johnteskemusic.com",
   "description": "A new series of dynamic compositions.",
-  "url": "http://vectorscores.org",
+  "url": "https://vectorscores.org",
   "links": [
     {
       "name": "johnteske/vectorscores",
       "url": "https://github.com/johnteske/vectorscores",
       "icon": "github"
     },
-    { "name": "johnteskemusic.com", "url": "http://johnteskemusic.com" }
+    { "name": "johnteskemusic.com", "url": "https://johnteskemusic.com" }
   ],
-  "baseUrl": process.env.VS_BASE_URL || "",
+  "baseUrl": process.env.SITE_BASE_URL || "",
   "assetsUrl": "assets"
 }
