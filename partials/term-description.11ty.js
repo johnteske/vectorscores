40808061347be7e6a0f61855f27467486746aaf6
John Teske <john.teske@protonmail.com>
2020-07-27 22:31:21 -0700

    Clean up and annotate glossary

--- partials/term-description.11ty.js
+++ partials/term-description.11ty.js
@@ -4,8 +4,9 @@ const glossary = requireRoot(`_data/glossary.json`);
 
 module.exports = (termName) => {
   const term = glossary.find((t) => t.name === termName);
+  // TODO this shouldn't need to be called
   if (typeof termName !== "string") {
-    return 'asd'
-  } 
+    return;
+  }
   return term.description;
 };
