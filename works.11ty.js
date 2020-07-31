c3e17ddb6d68ac3a94e1d187fccd68c033407ddd
John Teske <john.teske@protonmail.com>
2020-07-31 09:00:07 -0700

    Prettify works template

--- works.11ty.js
+++ works.11ty.js
@@ -4,28 +4,28 @@ const movementLayouts = ["movement"];
 module.exports = class {
   data() {
     return {
-      permalink: "works.json"
+      permalink: "works.json",
     };
   }
   render(data) {
-  const movements = data.collections.all.filter(f =>
-   movementLayouts.includes(f.data.layout)
-  )
-  const works = data.collections.all.filter(f =>
-    scoreLayouts.includes(f.data.layout)
-  )
-.map(w => {
-
-
-return {
-title: w.data.title,
-duration: w.data.duration,
-instrumentation: w.data.instrumentation,
-status: w.data.status,
-movements: movements.filter(m => m.url.includes(w.url))
-.map(m => m.data.title),
-url: w.url
-}});
-  return JSON.stringify(works)
+    const movements = data.collections.all.filter((f) =>
+      movementLayouts.includes(f.data.layout)
+    );
+    const works = data.collections.all
+      .filter((f) => scoreLayouts.includes(f.data.layout))
+      .map((w) => {
+        return {
+          title: w.data.title,
+          duration: w.data.duration,
+          instrumentation: w.data.instrumentation,
+          status: w.data.status,
+          formats: w.data.formats,
+          movements: movements
+            .filter((m) => m.url.includes(w.url))
+            .map((m) => m.data.title),
+          url: w.url,
+        };
+      });
+    return JSON.stringify(works);
   }
 };
