a9a791223712fd0f08910cec0b44ac19b3850f65
John Teske <john.teske@protonmail.com>
2020-07-30 18:50:32 -0700

    Return movement titles

---
+++ works.11ty.js
@@ -0,0 +1,31 @@
+const scoreLayouts = ["score", "score-set"];
+const movementLayouts = ["movement"];
+
+module.exports = class {
+  data() {
+    return {
+      permalink: "works.json"
+    };
+  }
+  render(data) {
+  const movements = data.collections.all.filter(f =>
+   movementLayouts.includes(f.data.layout)
+  )
+  const works = data.collections.all.filter(f =>
+    scoreLayouts.includes(f.data.layout)
+  )
+.map(w => {
+
+
+return {
+title: w.data.title,
+duration: w.data.duration,
+instrumentation: w.data.instrumentation,
+status: w.data.status,
+movements: movements.filter(m => m.url.includes(w.url))
+.map(m => m.data.title),
+url: w.url
+}});
+  return JSON.stringify(works)
+  }
+};
