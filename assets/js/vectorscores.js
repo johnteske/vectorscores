4ddd354e2516c8624600e56a62b47bb40e320f73
John Teske <john.teske@protonmail.com>
2020-12-05 09:08:27 -0800

    Add fallback WebSocket module to log only
    This simplifies the need to check per score if the module is
    available.

--- assets/js/vectorscores.js
+++ assets/js/vectorscores.js
@@ -1,19 +1,29 @@
-import * as util from './vs/helpers.util.js';
-import * as math from './vs/helpers.math.js';
-import * as hooks from './vs/hooks.js';
-import * as layout from './vs/layout.js';
-import * as score from './vs/score.js';
-import * as scoreOptions from './vs/score-options.js';
-import * as controls from './vs/controls.js';
-import * as modals from './vs/modals.js';
+import * as util from "./vs/helpers.util.js";
+import * as math from "./vs/helpers.math.js";
+import * as hooks from "./vs/hooks.js";
+import * as layout from "./vs/layout.js";
+import * as score from "./vs/score.js";
+import * as scoreOptions from "./vs/score-options.js";
+import * as controls from "./vs/controls.js";
+import * as modals from "./vs/modals.js";
 
 window.VS = {
-    ...util,
-    ...math,
-    ...hooks,
-    ...layout,
-    ...score,
-    ...scoreOptions,
-    ...controls,
-    ...modals
+  ...util,
+  ...math,
+  ...hooks,
+  ...layout,
+  ...score,
+  ...scoreOptions,
+  ...controls,
+  ...modals,
+  WebSocket: {
+    connect: () => {
+      console.log("WebSocket module not available");
+    },
+    hooks: {
+      add: () => {
+        console.log("WebSocket module not available");
+      },
+    },
+  },
 };
