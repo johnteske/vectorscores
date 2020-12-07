3669d27d94011e19720946194708868e7f02d992
John Teske <john.teske@protonmail.com>
2020-12-06 17:28:22 -0800

    Do not copy _*.html files, copy dumpster SVG

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(config) {
   config.addPassthroughCopy("assets/js/lib/d3.v4.min.js");
   config.addPassthroughCopy("assets/modules");
 
-  config.addPassthroughCopy("scores/trashfire/dumpster.svg");
+  config.addPassthroughCopy({ "scores/trashfire/dumpster.svg": "trashfire/dumpster.svg" });
 
   const scoreLayouts = ["score", "score-set", "movement"];
   scoreLayouts.forEach(template =>
