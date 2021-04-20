75957d9a6dab0517201814d5f98684ef170de28c
John Teske <john.teske@protonmail.com>
2021-04-19 23:41:25 -0700

    Add save SVG hook
    This allows custom score options like resizing before export

--- assets/modules/save-svg/index.js
+++ assets/modules/save-svg/index.js
@@ -1,6 +1,12 @@
+VS.saveSVG = {
+  hooks: VS.createHooks(["save"]),
+};
+
 var element = document.getElementById("save-svg");
 
 element.onclick = function (e) {
+  VS.saveSVG.hooks.trigger("save");
+
   var svgXML = new XMLSerializer().serializeToString(d3.select("svg").node());
 
   element.setAttribute(
