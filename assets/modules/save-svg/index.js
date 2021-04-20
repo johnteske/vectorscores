e5cbcd9e1ad0a1e08e796ac1d01cd1f680f68886
John Teske <john.teske@protonmail.com>
2021-04-19 23:04:27 -0700

    Do not encode twice

--- assets/modules/save-svg/index.js
+++ assets/modules/save-svg/index.js
@@ -2,11 +2,10 @@ var element = document.getElementById("save-svg");
 
 element.onclick = function (e) {
   var svgXML = new XMLSerializer().serializeToString(d3.select("svg").node());
-  var encoded = encodeURI(svgXML);
 
   element.setAttribute(
     "href",
-    "data:text/svg;charset=utf-8," + encodeURIComponent(encoded)
+    "data:text/svg;charset=utf-8," + encodeURIComponent(svgXML)
   );
   element.setAttribute("download", "score.svg");
 };
