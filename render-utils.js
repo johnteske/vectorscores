5f86de6a3b87b61ee5ec5fb5ecdea01affe70b38
John Teske <john.teske@protonmail.com>
2021-04-23 00:48:41 -0700

    Remove render utils

--- render-utils.js
+++
@@ -1,8 +0,0 @@
-const movementsFromUrl = (url, data) =>
-  data.collections.all.filter(
-    (page) => page.data.layout === "movement" && page.url.includes(url)
-  );
-
-module.exports = {
-  movementsFromUrl,
-};
