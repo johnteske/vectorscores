082106470a488d1d103274458dffb74e990958aa
John Teske <john.teske@protonmail.com>
2021-04-23 08:26:58 -0700

    Upgrade d3 to v6 (#134)
    * Refactor script/style
    * Remove render utils
    * Replace committed d3.js with dependency

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,17 @@
-module.exports = function(config) {
+module.exports = function (config) {
   // copy assets that are not built to the target dir
-  config.addPassthroughCopy("assets/js/lib/d3.v4.min.js");
+  config.addPassthroughCopy({
+    "node_modules/d3/dist/d3.min.js": "assets/js/d3.min.js",
+  });
   config.addPassthroughCopy("assets/modules");
 
   const scoreLayouts = ["score", "score-set", "movement"];
-  scoreLayouts.forEach(template =>
+  scoreLayouts.forEach((template) =>
     config.addLayoutAlias(template, `layouts/${template}.11ty.js`)
   );
 
   config.setBrowserSyncConfig({
     codeSync: false,
-    ghostMode: false
+    ghostMode: false,
   });
 };
