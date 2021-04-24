7ad8b90227103484fb3ea442eeb1f7da0344cd13
John Teske <john.teske@protonmail.com>
2021-04-23 17:45:44 -0700

    Render bar from detached group element

--- scores/glauber/i/index.js
+++ scores/glauber/i/index.js
@@ -8,13 +8,33 @@ import makeScrollHelpers from "../../intonations/scroll-center";
 import addHooks from "../../intonations/scroll-hooks";
 
 //TODO VS.score events should have at least 1 event
-const score = [1, 2, 3];
+const score = [
+  {
+    render: () => {
+      const g = d3.create("svg:g");
+      g.append("rect").attr("width", "99").attr("height", "99");
+      return g;
+    },
+  },
+];
+
+function renderScore(selection, score) {
+  score.forEach((bar) => {
+    const { render, ...data } = bar;
+    const g = render(data);
+    selection.append(() => g.node());
+  });
+}
+
+//
+//
+//
 
 const { svg, page, scoreGroup, indicator } = makeScrollingScore();
 
 const { setScorePosition, scrollToNextBar } = makeScrollHelpers(
   scoreGroup,
-  score.map((_) => _) // score x values
+  score.map((_) => 0) // score x values
 );
 
 score.forEach((bar, i) => {
@@ -35,8 +55,7 @@ const resize = makeResize(
 d3.select(window).on("resize", resize);
 
 d3.select(window).on("load", () => {
-  console.log("todo");
-  //  renderScore();
+  renderScore(scoreGroup.element, score);
   resize();
 });
 
