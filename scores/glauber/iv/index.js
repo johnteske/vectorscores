7bdadac6dc39aeec4d179b08d936020e44a198d8
John Teske <john.teske@protonmail.com>
2021-04-30 17:36:09 -0700

    Create 'iv' script with sonata form

---
+++ scores/glauber/iv/index.js
@@ -0,0 +1,6 @@
+import * as sonataForm from "../sonata-form";
+
+const form = sonataForm.generate();
+
+// use sonata form again--but in scrolling, cards? vignettes?
+document.write(JSON.stringify(form))
