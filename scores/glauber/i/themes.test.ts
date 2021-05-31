5a4e462095db16599808000d5e4dbf8ccdfe051f
John Teske <john.teske@protonmail.com>
2021-05-31 11:23:38 -0700

    Use ts to aid test imports

---
+++ scores/glauber/i/themes.test.ts
@@ -0,0 +1,12 @@
+import test from "tape";
+
+import themes from "./themes";
+
+test("glauber i themes", function (t) {
+  // given an array of 100 integers and 0.25 disparity,
+  // expect the min/max to be 25 apart
+  // input: attr scale, disp
+  t.equal(false, false, "test name");
+
+  t.end();
+});
