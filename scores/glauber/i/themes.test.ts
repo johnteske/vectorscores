e5bff46c6615c28f9a82725b2655ed91ff3da9b7
John Teske <john.teske@protonmail.com>
2021-05-31 20:35:56 -0700

    Add tests, adjust logic

--- scores/glauber/i/themes.test.ts
+++ scores/glauber/i/themes.test.ts
@@ -1,12 +1,38 @@
 import test from "tape";
 
-import themes from "./themes";
+import { fromDisp } from "./themes";
 
-test("glauber i themes", function (t) {
+const cont = Array.from({ length: 100 }, (x, i) => i);
+
+test(__dirname + " disparity", function (t) {
   // given an array of 100 integers and 0.25 disparity,
   // expect the min/max to be 25 apart
-  // input: attr scale, disp
-  t.equal(false, false, "test name");
+  const [min0, max0] = fromDisp(cont, 0.25);
+  t.equal(max0 - min0, 25, "disparity accuracy");
+
+  t.throws(
+    () => {
+      fromDisp([0, 1, 2], 1.1);
+    },
+    /must be between/,
+    "throws on disparity > 1"
+  );
+  t.throws(
+    () => {
+      fromDisp([0, 1, 2], -12);
+    },
+    /must be between/,
+    "throws on disparity < 0"
+  );
+
+  const [min1, max1] = fromDisp([0, 1, 2], 0);
+  t.ok(min1 === max1, "no disparity");
+
+  // TODO test sample size to see if both occur (but low sample size)
+  const [min, max] = fromDisp([0, 1, 2], 0.5);
+  t.ok((min === 0 && max === 1) || (min === 1 && max === 2));
+
+  t.deepEquals(fromDisp([0, 1, 2], 1), [0, 2], "max disparity");
 
   t.end();
 });
--- scores/glauber/i/themes.ts
+++ scores/glauber/i/themes.ts
@@ -1,45 +1,53 @@
-// make this available for variations
-// it prob just dones't belong here tho
-export const attributes = {
-  duration: [0.25, 0.5, 1, 2, 4, 8],
-  range: [16, 32, 64, 127], // can be much more granular than this
-  timbre: ["glassy", "ord", "sul pont"],
-  dynamics: ["pp", "p", "mp", "mf", "f", "ff"],
-};
+import { lcg, integerBetween } from "../../intonations/prng";
 
-const attributeKeys = Object.keys(attributes);
+//// make this available for variations
+//// it prob just dones't belong here though
+//export const attributes = {
+//  duration: [0.25, 0.5, 1, 2, 4, 8],
+//  range: [16, 32, 64, 127], // can be much more granular than this
+//  timbre: ["glassy", "ord", "sul pont"],
+//  dynamics: ["pp", "p", "mp", "mf", "f", "ff"],
+//};
+//
+//const attributeKeys = Object.keys(attributes);
+//
+//// between 0-1
+//const attributeDisparities = shuffle([1.0, 0.75, 0.5, 0.25]);
 
-// between 0-1
-const attributeDisparities = shuffle([1.0, 0.75, 0.5, 0.25]);
+const rando = lcg(Date.now());
 
-const withDisp = shuffle(attributeKeys).reduce((acc, key, i) => {
-  const scale = attributes[key];
-  const disparity = attributeDisparities[i];
-  const range = Math.round(scale.length * disparity); // TODO place this within the scale
-  const start = scale.length - range;
-  console.log(start, range, scale.length);
+export function fromDisp<T>(scale: T[], disparity: number): [T, T] {
+  if (disparity < 0 || disparity > 1) {
+    throw new Error("disparity must be between [0, 1] (inclusive)");
+  }
+  const range = Math.round((scale.length - 1) * disparity);
+  const start = integerBetween(rando, 0, scale.length - range);
   const min = scale[start];
   const max = scale[start + range];
+  return [min, max];
+}
 
-  return {
-    ...acc,
-    [key]: {
-      disparity,
-      scale,
-      min,
-      max,
-    },
-  };
-}, {});
-console.log(withDisp);
+//const withDisp = shuffle(attributeKeys).reduce((acc, key, i) => {
+//  const scale = attributes[key];
+//  const disparity = attributeDisparities[i];
+//  const [min, max] = fromDisp(scale, disparity);
+//  return {
+//    ...acc,
+//    [key]: {
+//      disparity,
+//      scale,
+//      min,
+//      max,
+//    },
+//  };
+//}, {});
+//console.log(withDisp);
 
 // object.freeze
 const themes = {};
 export default themes;
 
-//
-
 // TODO NOT A REAL FN
-function shuffle(a) {
-  return a.sort();
-}
+//function shuffle(a) {
+//  return a.sort();
+//}
